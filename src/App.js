c905462a4bf497b725150152b6950b6b5e15d1de
YOUR NAME <solisamo6@gmail.com>
2019-12-16 22:24:19 -0600

    Create a badgesList
    BadgesList was created to
    view all the data, and create
    some list.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css"
+
+import Badges from './pages/Badges'
+
+
+function App() {
+  return (
+    <div>
+      <Badges/>
+    </div>
+  );
+}
+
+export default App;
